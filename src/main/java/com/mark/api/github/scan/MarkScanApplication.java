8bd304cad69895ff956ed16333b762779ef90ce2
immark007 <immark5507@gmail.com>
2025-01-26 11:24:07 -0300

    feat: configuração inicial

---
+++ src/main/java/com/mark/api/github/scan/MarkScanApplication.java
@@ -0,0 +1,13 @@
+package com.mark.api.github.scan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkScanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mark/api/github/scan/MarkScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.api.github.scan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
